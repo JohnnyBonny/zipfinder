c8cd397f59fc28219e4ad59e07dcadff5cbaed42
Ethan Meade <ethanmeade@ucsb.edu>
2023-05-14 01:32:11 -0700

    EM - Fixed undesirable sliding in header and footer

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import {
   Input,
   Flex,
   Grid,
+  GridItem,
   Button,
 } from "@chakra-ui/react";
 
@@ -16,59 +17,69 @@ function App() {
   return (
     <div className="App">
       <ChakraProvider theme={theme}>
-        <Heading textAlign="center" padding="10px 0px 10px 0px" size="xl">
-          Zipfinder
-        </Heading>
-        <Spacer />
+        <Grid templateColumns="repeat(2, 4fr)" gap={1} padding="0% 15% 0% 15%">
+          <GridItem colSpan={2}>
+            <Heading textAlign="center" padding="10px 0px 10px 0px" size="xl">
+              Zipfinder
+            </Heading>
+            <Spacer />
 
-        <Flex justify="center" paddingBottom="10px">
-          <Input placeholder="Enter a zip code" size="lg" width="auto" />
-        </Flex>
-
-        <Grid templateColumns="repeat(2, 2fr)" gap={1} padding="0% 15% 0% 15%">
-          <Category
-            name="🍽️ Food"
-            location="Location 1"
-            address="1111 Bruh Moment Drive"
-            rating="4"
-            price="$$"
-            isOpen="Open"
-            url="http://google.com"
-          />
-          <Category
-            name="🚲 Recreation"
-            location="Location 2"
-            address="1111 Bruh Moment Drive"
-            rating="4"
-            price="$$"
-            isOpen="Open"
-            url="http://facebook.com"
-          />
-          <Category
-            name="🎥 Arts & Entertainment"
-            location="Location 3"
-            address="1111 Bruh Moment Drive"
-            rating="4"
-            price="$$$"
-            isOpen="Open"
-            url="http://linkedin.com"
-          />
-          <Category
-            name="🛍️ Shopping"
-            location="Location 4"
-            address="1111 Bruh Moment Drive"
-            rating="4"
-            price="$"
-            isOpen="Open"
-            url="http://twitter.com"
-          />
+            <Flex justify="center" paddingBottom="10px">
+              <Input placeholder="Enter a zip code" size="lg" width="auto" />
+            </Flex>
+          </GridItem>
+          <GridItem>
+            <Category
+              name="🍽️ Food"
+              location="Location 1"
+              address="1111 Bruh Moment Drive"
+              rating="4"
+              price="$$"
+              isOpen="Open"
+              url="http://google.com"
+            />
+          </GridItem>
+          <GridItem>
+            <Category
+              name="🚲 Recreation"
+              location="Location 2"
+              address="1111 Bruh Moment Drive"
+              rating="4"
+              price="$$"
+              isOpen="Open"
+              url="http://facebook.com"
+            />
+          </GridItem>
+          <GridItem>
+            <Category
+              name="🎥 Arts & Entertainment"
+              location="Location 3"
+              address="1111 Bruh Moment Drive"
+              rating="4"
+              price="$$$"
+              isOpen="Open"
+              url="http://linkedin.com"
+            />
+          </GridItem>
+          <GridItem>
+            <Category
+              name="🛍️ Shopping"
+              location="Location 4"
+              address="1111 Bruh Moment Drive"
+              rating="4"
+              price="$"
+              isOpen="Open"
+              url="http://twitter.com"
+            />
+          </GridItem>
+          <GridItem colSpan={2}>
+            <Flex align="center" justify='center'>
+              <Button color="white" colorScheme="teal" size="lg" justifyContents='flex-start'>
+                Refresh all locations
+              </Button>
+            </Flex>
+          </GridItem>
         </Grid>
-
-        <div align="center">
-          <Button justify="center" color="white" colorScheme="teal" size="lg">
-            Refresh all locations
-          </Button>
-        </div>
       </ChakraProvider>
     </div>
   );
